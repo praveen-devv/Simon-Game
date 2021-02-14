a2b26dc3625f7411bef2b56d9fb61f38daf85ef7
praveen-devv <praveenmohan2408@gmail.com>
2021-02-14 23:25:02 +0530

    Initial Commit

---
+++ game.js
@@ -0,0 +1,104 @@
+var buttonColors =["red", "blue", "green", "yellow"];
+
+var gamePattern=[];
+
+var userClickedPattern=[];
+
+var started = false;
+
+var level=0;
+
+
+$(".btn").click(function() {
+
+  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
+
+  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+
+});
+
+$(document).keypress(function(event){
+  if(!started)
+  {
+    $("#level-title").text("level" + level);
+    nextSequence();
+    started=true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamePattern.length){
+
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+
+    }
+
+}
+
+function startOver()
+{
+  level=0;
+  gamePattern=[];
+  started=false;
+}
+
+
+function nextSequence()
+{
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber=Math.floor(Math.random()*4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColor);
+
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
